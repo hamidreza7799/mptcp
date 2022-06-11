d7f0f8c16d65ecd37d43a9c8cd0134b3f9f40d27
hamidreza <hamidreza.azarbad77@gmail.com>
2022-06-11 13:07:15 +0000

    add all results

--- balia_ndiffports_rr/run.sh
+++ balia_ndiffports_rr/run.sh
@@ -2,7 +2,9 @@
 COUNTER=0
 while [ "$COUNTER" -lt 50 ]
 do
-   mprun -t topo -x iperf_senario
+   sudo python /home/vagrant/minitopo/runner.py -t topo -x iperf_scenario
+   echo $COUNTER
+   echo "Finish"
    IPERFCOUNTER=IPERF_${COUNTER}
    mv iperf.log0 $IPERFCOUNTER
    COUNTER=`expr $COUNTER + 1`
